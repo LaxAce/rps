40b8b2dbfba85a74dfa42800fc03f776f4e3e3ee
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2022-06-04 13:03:45 +0100

    make it better

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ const playRound = (playerSelection, computerSelection) => {
   // Scissors & Paper
   if (inputToLowercase == "scissors" && computerSelection == "paper")
     return "You Won! Scissors beats Paper";
-  if (computerSelection == "scissors" && inputToLowercase == "paper")
+  else if (computerSelection == "scissors" && inputToLowercase == "paper")
     return "You Lose! Scissors beats Paper";
 
   // Paper & Rock
   if (inputToLowercase == "paper" && computerSelection == "rock")
     return "You Won! Paper beats Rock";
-  if (computerSelection == "paper" && inputToLowercase == "rock")
+  else if (computerSelection == "paper" && inputToLowercase == "rock")
     return "You Lose! Paper beats Rock";
 
   // Rock & Scissors
   if (inputToLowercase == "rock" && computerSelection == "scissors")
     return "You Won! Rock beats Scissors";
-  if (computerSelection == "rock" && inputToLowercase == "scissors")
+  else if (computerSelection == "rock" && inputToLowercase == "scissors")
     return "You Lose! Rock beats Scissors";
 
   // Draw
@@ -41,27 +41,32 @@ const playRound = (playerSelection, computerSelection) => {
 };
 
 const game = () => {
-    let yourScore = 0;
-    let computerScore = 0;
+  let yourScore = 0;
+  let computerScore = 0;
 
   for (let i = 0; i < 5; i++) {
     let playerSelection = prompt(
       "Make a wise choice! Rock, Paper, or Scissors"
     );
-    
+
     const computerSelection = computerPlay();
     const result = playRound(playerSelection, computerSelection);
-    if(result.includes('Won')) yourScore++
-    if(result.includes('Lose')) computerScore++
-    console.log(result)
-    console.log( `Current Score = you: ${yourScore} - computer: ${computerScore}`)
-    console.log(' ')
+    if (result.includes("Won")) yourScore++;
+    else if (result.includes("Lose")) computerScore++;
+    console.log(result);
+    console.log(
+      `Current Score = you: ${yourScore} - computer: ${computerScore}`
+    );
+    console.log(" ");
   }
 
-  console.log(' ')
-  if (yourScore > computerScore) console.log('**********YOU WON THE GAME**********')
-  if (computerScore > yourScore) console.log('**********COMPUTER WON THE GAME**********')
-  if (yourScore == computerScore) console.log('******** DRAW!! ************')
+  console.log(" ");
+  if (yourScore > computerScore)
+    console.log("**********YOU WON THE GAME**********");
+  else if (computerScore > yourScore)
+    console.log("**********COMPUTER WON THE GAME**********");
+  else if (yourScore == computerScore)
+    console.log("******** DRAW!! ************");
 };
 
 game();
